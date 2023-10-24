718284b2aebd53e0d04484f63fa14480ed573ba5
mert çobanoğlu <mertcbb@gmail.com>
2023-10-25 01:44:36 +0300

    Added auth guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { LoginComponent } from '../admin/components/login/login.component';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  if (auth.isLoggedIn()) {
+    if (route.component == LoginComponent) {
+      router.navigate(['admin', 'dashboard'])
+    }
+    return true;
+  }
+  else {
+    router.navigate(['admin', 'login'])
+    return false
+  }
+};
