6fec66a443ceaaa2d1850415087ff998d8234275
mert çobanoğlu <mertcbb@gmail.com>
2023-10-25 15:30:03 +0300

    Added admin module and layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UiModule } from './ui/ui.module';
+import { UiComponent } from './ui/ui.component';
+import { AdminModule } from './admin/admin.module';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => UiModule },
+  { path: '', component: UiComponent, loadChildren: () => UiModule },
+  { path: 'admin', component: AdminComponent, loadChildren: () => AdminModule }
 ];
 
 @NgModule({
