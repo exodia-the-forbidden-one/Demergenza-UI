3f95dd2f29de72b93c903e1cb26e8eaf3bdc0ce7
mert çobanoğlu <mertcbb@gmail.com>
2024-01-09 22:06:06 +0300

    Modified About Us

--- src/app/ui/components/about-us/about-us.component.ts
+++ src/app/ui/components/about-us/about-us.component.ts
@@ -3,14 +3,18 @@ import { ContentReaderService } from 'src/app/services/Repositories/content-read
 import { AboutUsModel } from 'src/app/models/about-us.model';
 import { HttpErrorResponse } from '@angular/common/http';
 
+import { Breadcrumb } from 'primeng/breadcrumb';
+import { MenuItem } from 'primeng/api';
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent implements OnInit {
-  constructor(private contentReader: ContentReaderService, ) { }
+  constructor(private contentReader: ContentReaderService,) { }
   aboutUsContent: AboutUsModel;
+  items: MenuItem[];
 
   ngOnInit(): void {
     this.contentReader.GetAboutUsContent().subscribe({
@@ -21,6 +25,11 @@ export class AboutUsComponent implements OnInit {
         console.log(err.message);
       }
     })
+
+    this.items = [
+      { label: "Ana Sayfa", url: "/" },
+      { label: "Hakkımızda", url: "hakkimizda" }
+    ];
 
   }
 }
